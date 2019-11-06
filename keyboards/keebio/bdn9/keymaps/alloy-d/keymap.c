b172d59fda28aab07db7234abda11602e698e70c
Adam Lloyd <adam@alloy-d.net>
2019-11-05 20:14:14 -0500

    Add a bdn9 layer for navigating in tmux.

--- keyboards/keebio/bdn9/keymaps/alloy-d/keymap.c
+++ keyboards/keebio/bdn9/keymaps/alloy-d/keymap.c
@@ -39,10 +39,26 @@
 float song1[][2] = SONG(MUSIC_SCALE_SOUND);
 float song4[][2] = SONG(BALL_GAME);
 
+#define TMUX_PREFIX SS_LCTRL("x")
+#define SEND_TMUX_PREFIX SEND_STRING(TMUX_PREFIX)
+#define SEND_TMUX_COMMAND(cmd) SEND_STRING(TMUX_PREFIX cmd)
+
 enum custom_keycodes {
     SHIFT_ESC = SAFE_RANGE,
-    TMUX_COPY,
-    LOCK
+    LOCK,
+
+    TMUX_ENTER_COPY_MODE,
+    TMUX_UP,
+    TMUX_DOWN,
+    TMUX_LEFT,
+    TMUX_RIGHT,
+    TMUX_NEXT_PANE,
+    TMUX_PREV_PANE
+};
+
+enum layers {
+    BASE = 0,
+    TMUX
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,11 +68,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         | Hold: Layer 2     | Up   | RGB Mode           |
         | Left              | Down | Right              |
      */
-    [0] = LAYOUT(
-        KC__MUTE,   KC_PGUP, TMUX_COPY,
-        LOCK,       KC_PGDN, KC_ESC,
-        SHIFT_ESC,  KC_F23,  KC_F24
+    [BASE] = LAYOUT(
+        KC__MUTE,   KC_PGUP, TMUX_ENTER_COPY_MODE,
+        SHIFT_ESC,  KC_PGDN, KC_ESC,
+        MO(TMUX),   KC_F24,  LOCK
     ),
+
+    [TMUX] = LAYOUT(
+        _______,    TMUX_UP,        _______,
+        TMUX_LEFT,  TMUX_DOWN,      TMUX_RIGHT,
+        _______,    TMUX_PREV_PANE, TMUX_NEXT_PANE
+    ),
+
     /*
         | RESET          | N/A  | Media Stop |
         | Held: Layer 2  | Home | RGB Mode   |
@@ -80,24 +103,53 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // We'll only operate on the press event below, so let's save
+    // ourselves a bunch of repeated if statements.
+    if (!record->event.pressed) {
+        return true;
+    }
+
     switch (keycode) {
         case LOCK:
-            if (!record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_LCTRL("q")));
-            }
+            SEND_STRING(SS_LGUI(SS_LCTRL("q")));
             return false;
         case SHIFT_ESC:
-            if (!record->event.pressed) {
-                register_code(KC_LSHIFT);
-                tap_code(KC_ESCAPE);
-                unregister_code(KC_LSHIFT);
-            }
+            register_code(KC_LSHIFT);
+            tap_code(KC_ESCAPE);
+            unregister_code(KC_LSHIFT);
             return false;
-        case TMUX_COPY:
-            if (!record->event.pressed) {
-                SEND_STRING(SS_LCTRL("x")"[");
-            }
+
+        case TMUX_ENTER_COPY_MODE:
+            SEND_TMUX_COMMAND("[");
             return false;
+
+        case TMUX_UP:
+            SEND_TMUX_PREFIX;
+            tap_code(KC_UP);
+            return false;
+        case TMUX_DOWN:
+            SEND_TMUX_PREFIX;
+            tap_code(KC_DOWN);
+            return false;
+        case TMUX_LEFT:
+            SEND_TMUX_PREFIX;
+            tap_code(KC_LEFT);
+            return false;
+        case TMUX_RIGHT:
+            SEND_TMUX_PREFIX;
+            tap_code(KC_RIGHT);
+            return false;
+
+        case TMUX_NEXT_PANE:
+            SEND_TMUX_PREFIX;
+            tap_code(KC_SPACE);
+            return false;
+        case TMUX_PREV_PANE:
+            SEND_TMUX_PREFIX;
+            tap_code(KC_BSPACE);
+            return false;
+
+
         default:
             return true;
     }
