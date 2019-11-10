352bb921a88172d86df8666ad6283ebc6e4aae0c
Adam Lloyd <adam@alloy-d.net>
2019-11-09 20:08:42 -0500

    Add leader key and use it to toggle persistent numpad.

--- keyboards/planck/keymaps/alloy-d/keymap.c
+++ keyboards/planck/keymaps/alloy-d/keymap.c
@@ -56,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/" |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Hyper| Ctrl | Alt  | GUI  |Lower |Num/Sp|ce/Nav|Raise | GUI  | Alt  | Ctrl |Hyper |
+ * | Hyper| Ctrl | Alt  | GUI  |Lower |Num/Sp|ce/Nav|Raise | GUI  | Alt  | Ctrl | Lead |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     C_T_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT ,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, S_T_QUO,
-    KC_HYPR, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   NUM_SPC, NAV_SPC, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_HYPR
+    KC_HYPR, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   NUM_SPC, NAV_SPC, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_LEAD
 ),
 
 /* Colemak
@@ -337,7 +337,21 @@ void dip_switch_update_user(uint8_t index, bool active) {
     }
 }
 
+LEADER_EXTERNS();
 void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
+
+        /* Leader N / Leader 4: toggle numpad layer. */
+        SEQ_ONE_KEY(KC_J /* Colemak N */) {
+            layer_on(_NUM);
+        }
+        SEQ_ONE_KEY(KC_KP_4) {
+            layer_off(_NUM);
+        }
+    }
+
 #ifdef AUDIO_ENABLE
     if (muse_mode) {
         if (muse_counter == 0) {
