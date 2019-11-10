71c0f7c99cec55522fa327d826b93b180967a112
Adam Lloyd <adam@alloy-d.net>
2019-11-10 12:18:50 -0500

    What encoder?

--- keyboards/planck/keymaps/alloy-d/keymap.c
+++ keyboards/planck/keymaps/alloy-d/keymap.c
@@ -283,38 +283,6 @@ uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
 
-void encoder_update(bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      #ifdef MOUSEKEY_ENABLE
-        tap_code(KC_MS_WH_DOWN);
-      #else
-        tap_code(KC_PGDN);
-      #endif
-    } else {
-      #ifdef MOUSEKEY_ENABLE
-        tap_code(KC_MS_WH_UP);
-      #else
-        tap_code(KC_PGUP);
-      #endif
-    }
-  }
-}
-
 void dip_switch_update_user(uint8_t index, bool active) {
     switch (index) {
         case 0: {
