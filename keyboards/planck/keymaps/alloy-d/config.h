13b3fad59bdda6fdc811ab3c277c45b7153235c6
Adam Lloyd <adam@alloy-d.net>
2019-11-12 07:10:38 -0500

    Make the startup sound a pleasant little chirp.

--- keyboards/planck/keymaps/alloy-d/config.h
+++ keyboards/planck/keymaps/alloy-d/config.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
+    //#define STARTUP_SONG SONG(PLANCK_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
+    #define STARTUP_SONG SONG(E__NOTE(_C8), E__NOTE(_G8))
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
                                   SONG(COLEMAK_SOUND), \
