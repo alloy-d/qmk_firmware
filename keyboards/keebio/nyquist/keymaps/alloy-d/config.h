00fa9dba9e7644e23ad391bc5b33382ccea7693d
Adam Lloyd <adam@alloy-d.net>
2019-11-25 19:42:47 -0500

    Move Planck config into userspace and generalize to cover Nyquist.

---
+++ keyboards/keebio/nyquist/keymaps/alloy-d/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Used by my userspace code to strip the top row of layouts.
+#define ORTHO_5X12
+
+#ifdef AUDIO_ENABLE
+    //#define STARTUP_SONG SONG(PLANCK_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+    #define STARTUP_SONG SONG(E__NOTE(_C8), E__NOTE(_G8))
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(COLEMAK_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }
+#endif
---
+++ users/alloy-d/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* Makes mod-tap keys work the way I'd expect for fast typing. */
+#define PERMISSIVE_HOLD
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+
+#define LEADER_TIMEOUT 500
+#define LEADER_KEY_STRICT_KEY_PROCESSING
