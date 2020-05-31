c4748f3f9d4a5acecfee511600ece9f7c0747a5e
Adam Lloyd <adam@alloy-d.net>
2020-05-31 11:20:44 -0400

    Get rid of leader, move tmux copy to the right, refactor F-keys.

--- users/alloy-d/ortho.c
+++ users/alloy-d/ortho.c
@@ -55,7 +55,6 @@ enum custom_keycodes {
 
 #define C_T_ESC LCTL_T(KC_ESC)
 #define S_T_QUO RSFT_T(KC_QUOT)
-#define NAV_SPC LT(_NAV, KC_SPC)
 #define SCRSHOT G(S(KC_4))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -70,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Sft/(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/) |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Hyper| Ctrl | Alt  | GUI  |Lower |Num/Sp|ce/Nav|Raise | GUI  | Alt  | Ctrl | Lead |
+ * | Hyper| Ctrl | Alt  | GUI  |Lower | XXXX |Space |Raise | GUI  | Alt  | Ctrl |Hyper |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_ortho(
@@ -78,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
     C_T_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT ,
     SC_LSPR, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPR,
-    KC_HYPR, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   NAV_SPC, KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_LEAD
+    KC_HYPR, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_NO,   KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_HYPR
 ),
 
 /* Colemak overrides
@@ -102,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-/* Lower
+/* Lower -- almost entirely overridden by NAV and SYM.
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - |
@@ -123,28 +122,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______,     KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
-/* Raise
+/* Raise -- composed with NAV
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * | Ins  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |SysReq|
  * |- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - |
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
+ * |   `  |  F1  |  F2  |  F3  |  F4  |      |      |      |      |      |      |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  '   |
+ * | Del  |  F5  |  F6  |  F7  |  F8  |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Sft/{ |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn | }/Sft|
+ * |Sft/{ |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |      | }/Sft|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |TG NAV|      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
-    SC_LSCB, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, SC_RSCB,
+    KC_INS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_SYSREQ,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, _______, KC_BSLS,
+    KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______, _______, _______, _______, _______,
+    SC_LSCB, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, SC_RSCB,
     _______, _______, _______, _______, _______, _______, TG(_NAV),_______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
-/* Symbols (&c)
+/* Symbols (&c) -- composed with LOWER
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - |
@@ -154,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Sft/[ |G-S-F4|  F5  |   \  |   _  |   `  |      |      |      |      |      | ]/Sft|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |      |             |      |      |      |      |  Ins |
  * `-----------------------------------------------------------------------------------'
  */
 [_SYM] = LAYOUT_ortho(
@@ -165,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS
 ),
 
-/* Num
+/* Num -- composed with LOWER
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - |
@@ -186,13 +185,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, KC_PLUS,      _______,KC_0,   KC_DOT, KC_ENTER,   _______
 ),
 
-/* Nav
+/* Nav -- composed with RAISE
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - |
  * |      |      |      |      |      |      | M-Lft| PgDn | PgUp |M-Rgt |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |tmuxcp|      |      |      |      |      | Left | Down |  Up  |Right |      |      |
+ * |      |      |      |      |      |      | Left | Down |  Up  |Right |      |tmuxcp|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      | End  | Home |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -202,7 +201,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NAV] = LAYOUT_ortho(
     _______, _______, _______, _______, _______, _______, _______,      _______,    _______,    _______, _______, _______,
     _______, _______, _______, _______, _______, _______, A(KC_LEFT),   KC_PGDN,    KC_PGUP, A(KC_RGHT), _______, _______,
-    TMUXCPY, _______, _______, _______, _______, _______, KC_LEFT,      KC_DOWN,    KC_UP  ,    KC_RGHT, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_LEFT,      KC_DOWN,    KC_UP  ,    KC_RGHT, _______, TMUXCPY,
     _______, _______, _______, _______, _______, _______, _______,      KC_END,     KC_HOME,    _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,      _______,    _______,    _______, _______, _______
 ),
@@ -235,9 +234,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - +- - - |
  * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |MUSmod|Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|      |Plover|      |
+ * |KeyLck|      |MUSmod|Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|      |Plover|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
+ * | Caps |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -245,8 +244,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_ortho(
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, CMK_OFF, CMK_ON,   _______, PLOVER,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+    KC_LOCK, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, CMK_OFF, CMK_ON,   _______, PLOVER,  _______,
+    KC_CAPS, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
 
@@ -263,6 +262,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         state |= (1UL << _SYM) | (1UL << _NUM);
     } else {
         state &= ~((1UL << _SYM) | (1UL << _NUM));
+    }
+
+    // Compose raise with the NAV layer.
+    if (state & (1UL << _RAISE)) {
+        state |= (1UL << _NAV);
+    } else {
+        state &= ~(1UL << _NAV);
     }
 
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
@@ -374,35 +380,7 @@ void dip_switch_update_user(uint8_t index, bool active) {
     }
 }
 
-LEADER_EXTERNS();
 void matrix_scan_user(void) {
-    LEADER_DICTIONARY() {
-        leading = false;
-        leader_end();
-
-        /* Leader N / Leader 4: toggle numpad layer. */
-        SEQ_ONE_KEY(KC_J /* Colemak N */) {
-            layer_on(_NUM);
-        }
-        SEQ_ONE_KEY(KC_KP_4) {
-            layer_off(_NUM);
-        }
-
-        /* Leader right space: toggle nav layer. */
-        SEQ_ONE_KEY(NAV_SPC) {
-            if (IS_LAYER_ON(_NAV)) {
-                layer_off(_NAV);
-            } else {
-                layer_on(_NAV);
-            }
-        }
-
-        /* Leader hyper: caps lock. */
-        SEQ_ONE_KEY(KC_HYPR) {
-            tap_code(KC_CAPS);
-        }
-    }
-
 #ifdef AUDIO_ENABLE
     if (muse_mode) {
         if (muse_counter == 0) {
@@ -431,13 +409,4 @@ bool music_mask_user(uint16_t keycode) {
     default:
       return true;
   }
-}
-
-uint16_t get_tapping_term(uint16_t keycode) {
-    switch(keycode) {
-        case NAV_SPC:
-            return TAPPING_TERM + 1000;
-        default:
-            return TAPPING_TERM;
-    }
 }
