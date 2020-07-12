54665de116b5afc27d5e7067ffa941ab949f8ca4
Adam Lloyd <adam@alloy-d.net>
2020-07-12 07:10:14 -0400

    Turn space back into space now that I've unlearned it.

--- users/alloy-d/ortho.c
+++ users/alloy-d/ortho.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Sft/(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/) |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Hyper| Ctrl | Alt  | GUI  |Lower | XXXX |Space |Raise | GUI  | Alt  | Ctrl |Hyper |
+ * | Hyper| Ctrl | Alt  | GUI  |Lower | Space|Space |Raise | GUI  | Alt  | Ctrl |Hyper |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_ortho(
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
     C_T_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT ,
     SC_LSPR, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPR,
-    KC_HYPR, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_NO,   KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_HYPR
+    KC_HYPR, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_HYPR
 ),
 
 /* Colemak overrides
