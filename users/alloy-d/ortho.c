640ed51b0b603ba236d657ab3a72b3fd9662e531
Adam Lloyd <adam@alloy-d.net>
2021-05-13 10:48:11 -0400

    Switch back raise and left space.

--- users/alloy-d/ortho.c
+++ users/alloy-d/ortho.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
     C_T_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT ,
     SC_LSPR, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPR,
-    KC_HYPR, SC_LCBR, SC_LACB, KC_LGUI, LOWER,   KC_SPC,  RAISE,   KC_SPC,  KC_RGUI, SC_RACB, SC_RCBR, KC_HYPR
+    KC_HYPR, SC_LCBR, SC_LACB, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, SC_RACB, SC_RCBR, KC_HYPR
 ),
 
 /* Colemak overrides
