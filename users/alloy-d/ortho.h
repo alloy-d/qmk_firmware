ec7d186e74736613d070aaf0b15b982adcc34781
Adam Lloyd <adam@alloy-d.net>
2021-05-27 07:58:22 -0400

    Add Helix.

--- users/alloy-d/ortho.h
+++ users/alloy-d/ortho.h
@@ -13,6 +13,22 @@ LAYOUT_ortho_4x12( \
     k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b  \
 )
 
+#elif defined(ORTHO_HELIX_4ROW)
+// FIXME: make this less hacky.
+#define LAYOUT_ortho( \
+    kx0, kx1, kx2, kx3, kx4, kx5, kx6, kx7, kx8, kx9, kxa, kxb, \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b  \
+) \
+LAYOUT( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, \
+    k30, k31, k32, k33, k34, k35, KC_SYSREQ, KC_INS, k36, k37, k38, k39, k3a, k3b  \
+)
+
 #elif defined(ORTHO_5X12)
 #define LAYOUT_ortho( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
