626df2191a4a66415b2784e6afd3a01608c733ed
Adam Lloyd <adam@alloy-d.net>
2021-09-17 08:17:48 -0400

    Change nav keys for Linux shortcuts instead of macOS.

--- keyboards/nullbitsco/nibble/keymaps/alloy-d/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/alloy-d/keymap.c
@@ -27,7 +27,7 @@ enum custom_keycodes {
 };
 
 // TODO
-// - encoder
+// - encoder: push to mute mic, turn to adjust volume
 // - colemak layer
 // - mute keys on left side?
 // - right super key?
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_FN] = LAYOUT_ansi(
              _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______, \
-    _______, KC_LCBR, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,A(KC_LEFT),KC_PGDN,KC_PGUP,A(KC_RGHT),_______,_______, _______, KC_RCBR, _______, \
+    _______, KC_LCBR, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,C(KC_LEFT),KC_PGDN,KC_PGUP,C(KC_RGHT),_______,_______, _______, KC_RCBR, _______, \
     _______, KC_BSPC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, _______, \
     _______, SC_LSBR, _______, _______, KC_BSLS, KC_UNDS, KC_GRV,  _______, KC_END,  KC_HOME, _______, _______, SC_RSBR,          _______, _______, \
     _______, _______, _______, _______,                   _______,                   _______, _______, _______, KC_MPRV,          KC_MPLY, KC_MNXT  \
