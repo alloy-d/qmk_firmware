1ec7a06e2e35dc76e81af96e199c0e7ee16a29f6
Adam Lloyd <adam@alloy-d.net>
2021-09-17 08:17:17 -0400

    Begin setting up Anna's Helix.

--- users/annalloyd/ortho.c
+++ users/annalloyd/ortho.c
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
     C_T_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT ,
     SC_LSPR, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPR,
-    XXXXXXX, SC_LCBR, SC_LACB, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, SC_RACB, SC_RCBR, XXXXXXX
+    XXXXXXX, SC_LCBR, SC_LACB, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, SC_RACB, SC_RCBR, KC_DEL
 ),
 
 /* Colemak overrides
