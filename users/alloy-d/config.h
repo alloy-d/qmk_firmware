b9f2581ca9a91630df774a81c5576044fcbc5980
Adam Lloyd <adam@alloy-d.net>
2023-09-11 10:29:25 -0400

    Tweak mod-tap settings.

--- users/alloy-d/config.h
+++ users/alloy-d/config.h
@@ -1,10 +1,9 @@
 #pragma once
 
 /* Makes mod-tap keys work the way I'd expect for fast typing. */
-#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
 
-#define TAPPING_TERM 200
-#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM 100
 
 #define LEADER_TIMEOUT 500
 #define LEADER_KEY_STRICT_KEY_PROCESSING
